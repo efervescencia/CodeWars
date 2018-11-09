52782385260bfa48259fb8c96bb5e9ab9015af86
Julio <juliointelek@hotmail.com>
2018-11-09 21:45:11 +0100

    sumar la cantidad de elementos de varios intervalos
    los intervalos pueden ser negativos y solaparse pero en ese caso
    los elementos solo se cuentan una vez, hay que responder adecuadamente
    ante intervalos vacios, o rangos nulos.
    
    mi solucion ha sido un array de booleanos, transformando los negativos
    en positivos.

---
+++ Interval.java
@@ -0,0 +1,15 @@
+package packCodeWars;
+
+public class Interval {
+	
+    public static int sumIntervals(int[][] intervals) {
+        // primero detectamos overlapping de todos los rangos
+        return -1;
+    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
